25bf764a605c15de31a7772e7377839238f2df99
Mingzhe Zou <zoumingzhe@qq.com>
2025-04-24 20:47:11 +0800

    使用xkits-file提供的文件下载器下载文件

--- xiaoya_downloader/download.py
+++ xiaoya_downloader/download.py
@@ -5,7 +5,7 @@ from threading import Thread
 from time import sleep
 from typing import List
 
-from xkits_logger import Logger
+from xkits_file import Downloader
 
 from xiaoya_downloader.alist import AListAPI
 from xiaoya_downloader.resources import File
@@ -13,7 +13,6 @@ from xiaoya_downloader.resources import Resources
 
 
 class Download():
-    CHUNK_SIZE: int = 1048576
 
     def __init__(self, resources: Resources, api: AListAPI):
         self.__resources: Resources = resources
@@ -30,27 +29,14 @@ class Download():
     def join(self, file: File) -> str:
         return join(self.resources.base_dir, file.path, file.name)
 
-    def handle(self, file: File):
-        from requests import get  # pylint:disable=import-outside-toplevel
+    def download(self, file: File):
+        file.update(-1)
+        self.resources.save()
 
-        with get(file.data, stream=True, timeout=180.0) as stream:
-            stream.raise_for_status()  # HTTPError
-            with open(path := self.join(file), "wb") as whdl:
-                file.update(-1)
-                self.resources.save()
-                Logger.stdout(f"Download {path} started")
-                for chunk in stream.iter_content(chunk_size=self.CHUNK_SIZE):
-                    if chunk:
-                        whdl.write(chunk)
-                size: int = self.api.fs.get(join(file.path, file.name))["data"]["size"]  # noqa:E501
-                if whdl.tell() == size:
-                    Logger.stdout_green(
-                        f"Download {path} size {size} fininshed")
-                    file.update(size)
-                else:
-                    Logger.stderr_red(f"Download {path} size {size} error")
-                    file.update(-size)
-                self.resources.save()
+        if (downloader := Downloader(file.data, self.join(file))).start():
+            # size: int = self.api.fs.get(join(file.path, file.name))["data"]["size"]  # noqa:E501
+            file.update(downloader.stat.stat.st_size)
+            self.resources.save()
 
     def daemon(self):
         delay: float = 15.0
@@ -66,7 +52,7 @@ class Download():
 
                 if len(todo) > 0:
                     for file in todo:
-                        self.handle(file)
+                        self.download(file)
                     todo.clear()
 
                 delay = max(5.0, delay * 0.9)

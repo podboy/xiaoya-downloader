ab3fb828abf97c4bc72baa2a55b0f6d970e10b41
Mingzhe Zou <zoumingzhe@outlook.com>
2025-04-25 21:51:19 +0800

    增强下载处理
    修复一些已知的问题

--- xiaoya_downloader/download.py
+++ xiaoya_downloader/download.py
@@ -1,11 +1,14 @@
 # coding:utf-8
 
+from os.path import exists
+from os.path import isfile
 from os.path import join
 from threading import Thread
 from time import sleep
 
 from alist_kits import FS
 from xkits_file import Downloader
+from xkits_file import FileStat
 from xkits_logger import Logger
 
 from xiaoya_downloader.resources import File
@@ -29,16 +32,53 @@ class Download():
     def join(self, file: File) -> str:
         return join(self.resources.base_dir, file.path, file.name)
 
-    def download(self, file: File):
-        file.update(-(size := self.fs_api.get(join(file.path, file.name))["size"]))  # noqa:E501
-        self.resources.save()
+    def download(self, file: File) -> bool:
+        expected_size: int = self.fs_api.get(join(file.path, file.name))["size"]  # noqa:E501
+        assert isinstance(expected_size, int), f"Unexpected type '{type(expected_size)}'"  # noqa:E501
+        downloader: Downloader = Downloader(file.data, self.join(file))
 
-        if (downloader := Downloader(file.data, self.join(file))).start():
-            if (file_size := downloader.stat.stat.st_size) == size:
-                file.update(file_size)
-                self.resources.save()
-            else:
-                Logger.stderr_red(f"Failed to download {file.name}, expected size {size} != {file_size}")  # noqa:E501
+        if not exists(downloader.path):
+            file.update(-expected_size)
+            self.resources.save()
+
+            if not downloader.start():
+                Logger.stdout_red(f"Failed to download {join(file.path, file.name)}")  # noqa:E501
+                return False
+
+        if not isfile(downloader.path):
+            Logger.stdout_red(f"Path '{downloader.path}' is not a regular file")  # noqa:E501
+            return False
+
+        if (actual_size := downloader.stat.stat.st_size) != expected_size:
+            Logger.stdout_red(f"Path '{downloader.path}' expected size {expected_size} != {actual_size}")  # noqa:E501
+            return False
+
+        file.update(expected_size)
+        self.resources.save()
+        return True
+
+    def execute(self, file: File) -> bool:
+        if file.size <= 0 or not exists(path := self.join(file)):
+            try:
+                if not self.download(file):
+                    return False
+            except Exception as e:  # pylint:disable=broad-exception-caught
+                Logger.stdout_red(f"Failed to download '{join(file.path, file.name)}': {e}")  # noqa:E501
+                return False
+
+        if file.size <= 0:
+            Logger.stdout_red(f"'{join(file.path, file.name)}' unexpected size '{file.size}'")  # noqa:E501
+            return False
+
+        if not exists(path) or not isfile(path):
+            Logger.stdout_red(f"Path '{path}' is not a regular file")  # noqa:E501
+            return False
+
+        if (stat := FileStat(path)).stat.st_size != file.size:
+            Logger.stdout_red(f"Path '{path}' expected size {file.size} != {stat.stat.st_size}")  # noqa:E501
+            return False
+
+        return True
 
     def daemon(self):
         delay: float = 15.0
@@ -47,8 +87,7 @@ class Download():
             try:
                 for node in self.resources:
                     for file in node:
-                        if file.size <= 0:
-                            self.download(file)
+                        self.execute(file)
                 delay = max(5.0, delay * 0.9)
             except Exception:  # pylint:disable=broad-exception-caught
                 import traceback  # pylint:disable=import-outside-toplevel

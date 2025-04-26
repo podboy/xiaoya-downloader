617f02c100a71856b9f072e6bf915ac7eff8a651
Mingzhe Zou <zoumingzhe@outlook.com>
2025-04-27 03:40:48 +0800

    发布0.12.0版本

--- xiaoya_downloader/attribute.py
+++ xiaoya_downloader/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xiaoya-downloader"
-__version__ = "0.11.1"
+__version__ = "0.12.0"
 __urlhome__ = "https://github.com/bondbox/xargproject/"
 __description__ = "Automatically created by xargproject."
 
